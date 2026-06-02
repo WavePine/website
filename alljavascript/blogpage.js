909d225d21b65fa33757928897d4aa6737e7afe5
Haajir <abrahamshaajir@gmail.com>
2026-06-02 16:50:56 +0100

    Add files via upload

--- alljavascript/blogpage.js
+++ alljavascript/blogpage.js
@@ -3,18 +3,18 @@ const hamMenu = document.querySelector('.ham-menu');
 const offMenu = document.querySelector('.off-screen-menu');
 const offLinks = document.querySelectorAll('.off-screen-menu a');
 
-// --- 1. Toggle Hamburger & Sidebar ---
+console.log("JS loaded");
+
 hamMenu.addEventListener('click', () => {
+    console.log("clicked");
     hamMenu.classList.toggle('active');
     offMenu.classList.toggle('active');
 });
 
-// --- 2. Auto-close Sidebar on Mobile ---
-// This ensures that when you click a link and it scrolls, 
-// the menu slides away so you can see the content.
 offLinks.forEach(link => {
     link.addEventListener('click', () => {
         hamMenu.classList.remove('active');
         offMenu.classList.remove('active');
     });
 });
+
